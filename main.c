6bee92a39992e7bacb1d68d1e6273c26f33191eb
Hiatus <matiasrivas3106@gmail.com>
2022-05-27 06:48:13 -0400

    changes

--- main.c
+++ main.c
@@ -7,23 +7,140 @@ int charAmt = 0;
 char *fileDir; 
 int tape[arraySize];
 FILE* ptr; 
+FILE* out;
 char fl[fileSize];
 int ind = 0; 
 
 /* Some useful ascii code values 
 
 10 = \n 
-97 = a 
 65 = A 
+66 = B
+67 = C
+68 = D 
+69 = E
+70 = F
+71 = G
+72 = H
+73 = I
+74 = J
+75 = K
+76 = L
+77 = M
+78 = N
+79 = O
+80 = P
+81 = Q
+82 = R
+83 = S
+84 = T
+85 = U
+86 = V
+87 = W
+88 = X
+89 = Y
+90 = Z
 
 */ 
 
+
+// Find the next closing bracket
+int findClosingBracket(int start) {
+    int i = start;
+    int open = 0;
+    while (fl[i] != '\0') {
+        if (fl[i] == '['){
+            open++;
+        }
+        if (fl[i] == ']') {
+            open--;
+        }
+        if (open == 0) {
+            return i;
+        }
+        i++;
+    }
+    return -1;
+}
+
+// Translates char into ascii code
+char asciiTranslate(char find){
+    for(int i = 0; i < 100; i++){
+        char a = i;
+        if (a == find){
+            return i;  
+        }
+    }
+}
+
+void runCode(int i, int j){
+    int lastBracket = 0; 
+    while (i < j) {
+        printf("Index:%d\nChar:%c\n", i, fl[i]); 
+        if (fl[i] == '+') {
+            printf("Increasing value at index %d by 1\n", ind);
+            tape[ind]++;
+            printf("Value is now %d\n", tape[ind]);
+        }
+        if (fl[i] == '-') {
+            printf("Decreasing value at index %d by 1\n", ind);
+            tape[ind]--;
+            printf("Value is now %d\n", tape[ind]);
+        }
+        if (fl[i] == '>') {
+            printf("Increasing index %d by 1\n", ind);
+            ind++;
+        }
+        if (fl[i] == '<') {
+            printf("Decreasing index %d by 1\n", ind);
+            ind--;
+        }
+        if (fl[i] == '.') {
+            fprintf(out, "%d", tape[ind]);
+        }
+        if (fl[i] == ',') {
+            fprintf(out, "%c", tape[ind]);
+        }
+        if (fl[i] == '[') {
+            lastBracket = i; 
+            runCode(i+1, findClosingBracket(i)-1);
+            i = findClosingBracket(i);
+        }
+        if (fl[i] == ']') {
+            printf("Heading back to [\n"); 
+            if (tape[ind]!=0){
+                i = lastBracket;
+            }       
+        } 
+        i++; 
+    }
+}
+
+// The main method 
 void main(int argc, char *argv[] )  {  
 
     printf("Amt of arguments: %d\n", argc); 
     printf("File directory: %s\n", argv[1]);
 
     fileDir = argv[1];  
+    
+    // If the input file doesn't exist specify that you need one 
+    if (argv[1] == NULL){
+       printf("Please enter a file to run\n");
+       return;
+    }
+
+    // Create new file it doesn't exist
+    if (argv[2] == NULL) {
+        out = fopen("out", "w");
+        printf("Output directory: out\n");
+
+    } else {
+        out = fopen(argv[2], "w");
+        printf("Output directory: %s\n", argv[2]);
+    }
+
+
     ptr = fopen(fileDir, "r"); 
 
     // Place the file contents into the fl string, fl stands for file contents --> fl 
@@ -33,50 +150,8 @@ void main(int argc, char *argv[] )  {
         charAmt++; 
     }
 
-    // This loops through all the characters in the .tam file 
-    i = 0; 
-    while(i < charAmt){
-        //printf("%c", fl[i]);  
-        if (fl[i] == '>'){
-            if (ind+1 >= arraySize){
-                ind = 0;
-            } else {
-                ind++; 
-            } 
-        }
-        if (fl[i] == '<'){
-            if (ind-1 < 0){
-                ind = arraySize-1; 
-            } else {
-                ind--; 
-            }
-        }
-        if (fl[i] == '+'){
-            tape[ind]++; 
-        }
-        if (fl[i] == '-'){
-            tape[ind]--; 
-        }
-        if (fl[i] == '.'){
-            printf("%i", tape[ind]);
-        }
-        if (fl[i] == ','){
-            printf("%c", tape[ind]); 
-        }
-        i++;
-        printf("Index: %d\n", ind);
-    }
+    // This loops through all the characters in the file 
 
-    // char find = 'A'; 
-    // for(int i = 0; i < 100; i++){
-    //     char a = i;
-    //     if (a == find){
-    //         printf("Found %c at %d", find, a); 
-    //     }
-    // }
-
-    
-  
-
+    runCode(0, charAmt); 
 
 } 
